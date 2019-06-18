e5308df4314b1572d7622b039ce4e520e0954090
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-06-18 21:36:58 +0600

    rapidapi

---
+++ rapidapi/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rapidapi
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                var result = numbersFacts();
+
+                Console.ReadLine();
+            }
+            static Task numbersFacts()
+            {
+                WebRequest request = WebRequest.Create("https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true");
+                request.Headers.Add("X-RapidAPI-Host", "numbersapi.p.rapidapi.com");
+                request.Headers.Add("X-RapidAPI-Key", "e56a56465dmshcab6dc055b50ba0p11d6bfjsn9d8753c65d94");
+
+
+                WebResponse response = request.GetResponse();
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+
+                        string line = reader.ReadToEnd();
+
+                        numbers results = JsonConvert.DeserializeObject<numbers>(line);
+                        Console.WriteLine("Факты даты " + results.Text);
+                        Console.WriteLine("Год " + results.year);
+                        Console.WriteLine("Число " + results.number);
+                        Console.WriteLine("Поиск " + results.found);
+                        Console.WriteLine("Тип " + results.type);
+                    }
+                }
+                response.Close();
+                Console.WriteLine("Запрос выполнен");
+                return Task.CompletedTask;  
+        }
+    }
+}
---
+++ rapidapi/numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rapidapi
+{
+    public class numbers
+    {
+        public string Text { get; set; }
+        public int year { get; set; }
+        public int number{ get; set; }
+        public bool found { get; set; }
+        public string type { get; set; }
+    }
+}
